e4e64c1f0ec68d8f52f6d638979baa8e91b6c66c
Simon Milord <simon.milord@gmail.com>
2024-10-27 16:48:52 -0400

    pomodoro timer version 1.0, need some refactor and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,203 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import pressSound from "./sounds/press.mp3";
+import alertSound from "./sounds/alert.mp3";
 
-function App() {
+/**
+ * 0. Create a basic layout and -- DONE
+ * 1. Make the background color change based on the state -- DONE
+ * 2. Create a timer that counts down from 25 minutes, 5mins and 20mins -- DONE
+ * 3. Create a button that starts / pauses the timer -- DONE
+ * 4. Make sure we see the state and time remaining in tab title -- DONE
+ * 5. Create a button that skips to the next state -- DONE
+ * 6. Create alert when timer is done -- DONE
+ * 7. Make the message dynamic on the page -- DONE
+ * 8. Add a sound when timer is done and when a button is clicked -- DONE
+ * 9. Style the page - DONE
+ * 10. Refactor the code
+ * 11. Create a README.md
+ */
+
+const TWENTY_FIVE_MINUTES = 25 * 60;
+const FIVE_MINUTES = 5 * 60;
+const TWENTY_MINUTES = 20 * 60;
+const pressAudio = new Audio(pressSound);
+const alertAudio = new Audio(alertSound);
+
+const App = () => {
+  const [timerState, setTimerState] = useState<string>("focus"); // focus, shortBreak, longBreak
+  const [backgroundColorClass, setBackgroundColorClass] =
+    useState<string>("background-focus");
+  const [timeRemaining, setTimeRemaining] =
+    useState<number>(TWENTY_FIVE_MINUTES);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  const appCSSClass = `App ${backgroundColorClass}`;
+  const callToActionMessage = `Time to ${
+    timerState === "focus"
+      ? "focus!"
+      : timerState === "shortBreak"
+      ? "take a short break!"
+      : "take a long break!"
+  }`;
+
+  useEffect(() => {
+    setBackgroundColorClass(
+      timerState === "focus"
+        ? "background-focus"
+        : timerState === "shortBreak"
+        ? "background-sbreak"
+        : "background-lbreak"
+    );
+    setTimeRemaining(
+      timerState === "focus"
+        ? TWENTY_FIVE_MINUTES
+        : timerState === "shortBreak"
+        ? FIVE_MINUTES
+        : TWENTY_MINUTES
+    );
+  }, [timerState]);
+
+  useEffect(() => {
+    return () => {
+      clearIntervalId(intervalId);
+    };
+  }, [intervalId]);
+
+  useEffect(() => {
+    const formattedTimeRemaining =
+      Math.floor(timeRemaining / 60) +
+      ":" +
+      String(timeRemaining % 60).padStart(2, "0");
+    document.title = `${formattedTimeRemaining} - ${callToActionMessage}`;
+  }, [timerState, timeRemaining, callToActionMessage]);
+
+  const handleChangeState = (event: any) => {
+    if (isRunning) {
+      setIsRunning(false);
+      clearIntervalId(intervalId);
+    }
+    setTimerState(event.target.id);
+    switch (event.target.id) {
+      case "focus":
+        setTimerState("focus");
+        setTimeRemaining(TWENTY_FIVE_MINUTES);
+        break;
+      case "shortBreak":
+        setTimerState("shortBreak");
+        setTimeRemaining(FIVE_MINUTES);
+        break;
+      case "longBreak":
+        setTimerState("longBreak");
+        setTimeRemaining(TWENTY_MINUTES);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleStartOrPause = () => {
+    pressAudio.play();
+    if (isRunning) {
+      clearIntervalId(intervalId);
+      setIsRunning(false);
+    } else {
+      const id = setInterval(() => {
+        setTimeRemaining((prevTime) => {
+          if (prevTime <= 1) {
+            clearInterval(id);
+            alertAudio.play();
+            return 0;
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+      setIntervalId(id);
+      setIsRunning(true);
+    }
+  };
+
+  const handleSkip = () => {
+    clearIntervalId(intervalId);
+    switch (timerState) {
+      case "focus":
+        setTimerState("shortBreak");
+        break;
+      case "shortBreak":
+        setTimerState("longBreak");
+        break;
+      case "longBreak":
+        setTimerState("focus");
+        break;
+      default:
+        break;
+    }
+
+    setIsRunning(false);
+  };
+
+  // Clears the interval and sets the intervalId to null.
+  const clearIntervalId = (id: NodeJS.Timeout | null) => {
+    if (id) {
+      clearInterval(id);
+      setIntervalId(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className={appCSSClass}>
+      <header>Pomodoro timer</header>
+      <main>
+        <div>
+          <button
+            id="focus"
+            className={`stateButton ${
+              timerState === "focus" ? "stateButton__focus--selected" : null
+            }`}
+            onClick={handleChangeState}
+          >
+            Focus
+          </button>
+          <button
+            id="shortBreak"
+            className={`stateButton ${
+              timerState === "shortBreak" ? "stateButton__shortBreak--selected" : null
+            }`}
+            onClick={handleChangeState}
+          >
+            Short Break
+          </button>
+          <button
+            id="longBreak"
+            className={`stateButton ${
+              timerState === "longBreak" ? "stateButton__longBreak--selected" : null
+            }`}
+            onClick={handleChangeState}
+          >
+            Long Break
+          </button>
+        </div>
+        <p className="timeRemaining">
+          {Math.floor(timeRemaining / 60)}:
+          {String(timeRemaining % 60).padStart(2, "0")}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <button
+            id="startPause"
+            className="triggerButton"
+            onClick={handleStartOrPause}
+          >
+            {isRunning ? "Pause" : "Start"}
+          </button>
+          <button id="skip" className="triggerButton" onClick={handleSkip}>
+            Skip
+          </button>
+        </div>
+        <p>{callToActionMessage}</p>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
